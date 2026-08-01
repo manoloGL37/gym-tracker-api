3de2c2ee90bbb250ce7da4b39027c755b47716c6
Manolo G <manolo.gongora1@gmail.com>
2026-08-01 15:57:34 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/dev/manuel/gymtracker_api/GymtrackerApiApplication.java
@@ -0,0 +1,13 @@
+package dev.manuel.gymtracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymtrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymtrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/manuel/gymtracker_api/GymtrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.manuel.gymtracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymtrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
